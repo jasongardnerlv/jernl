30f34c07671f4522fa1bf545f6f430bd4343d1fe
Jason Gardner <jason.gardner@kaseya.com>
2016-09-06 13:52:09 -0700

    make date format configurable

--- jernl_new_entry.py
+++ jernl_new_entry.py
@@ -6,9 +6,12 @@ class JernlnewentryCommand(sublime_plugin.TextCommand):
         def scrollToBottom():
             v = 0, self.view.layout_extent()[1]
             self.view.set_viewport_position(v)
+        dateFormat = "%d/%m/%Y";
+        if (self.view.settings().get("jernl-date-format") is not None):
+            dateFormat = self.view.settings().get("jernl-date-format");
         out = "\n";
         out = out + "**** ";
-        out = out + datetime.date.today().strftime("%d/%m/%Y");
+        out = out + datetime.date.today().strftime(dateFormat);
         out = out + " ********************************************************************************************************";
         out = out + "\n\n  > ";
         self.view.insert(edit, self.view.sel()[0].begin(), out);
